71e69a151d5c1b0c635591656709c1b79602dc63
JohnSweet <john.sweet@charter.com>
2018-04-23 19:36:42 -0600

    Initial Commit

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div id="app"></div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
